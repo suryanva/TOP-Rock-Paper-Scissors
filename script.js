dc4c96ff1c58514dc279cd177c0f8e9e47894968
Varma S <varma.surya@protonmail.com>
2022-07-01 15:57:46 +0530

    added two variable to keep score to annonce winner after playing 5 times

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let userScore = 0;
+let compScore = 0;
+
 function computerPlay() {
   const choiceArray = ["rock", "paper", "scissors"];
   const randomChoice = Math.floor(Math.random() * 3);
@@ -10,16 +13,22 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     return "It's a tie";
   } else if (playerSelection == "rock" && computerSelection == "paper") {
+    compScore++;
     return "You Lose! Paper beats Rock";
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    userScore++;
     return "You Win! Rock beats Scissors";
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    compScore++;
     return "You Lose! Scissors beats paper";
   } else if (playerSelection == "paper" && computerSelection == "rock") {
+    userScore++;
     return "You Win! Paper beats Rock";
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    compScore++;
     return "You Lose! Rock beats scissors";
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    userScore++;
     return "You Win! Scissors beats Paper";
   }
 }
