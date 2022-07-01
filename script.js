c8f658f7cc16fb258d7797c0c5501b3c3e938774
Varma S <varma.surya@protonmail.com>
2022-07-01 15:51:35 +0530

    made a function which takes both user input and machine input and displays a winner

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function computerPlay() {
   const computerChoice = choiceArray[randomChoice];
   return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+  if (playerSelection == computerSelection) {
+    return "It's a tie";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You Lose! Scissors beats paper";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You Lose! Rock beats scissors";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You Win! Scissors beats Paper";
+  }
+}
