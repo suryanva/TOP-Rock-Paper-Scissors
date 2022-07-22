ba5cf22cb6d24f28e7ecff7636c71fb4fc157669
Varma S <varma.surya@protonmail.com>
2022-07-22 20:25:20 +0530

    added new UI Where buttons to the work of player section and initite the play

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 let userScore = 0;
 let compScore = 0;
 
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const outcomeButton = document.getElementById("outcome");
+
+rockButton.addEventListener("click", function () {
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+  const displayUserScore = document.getElementById("user-score");
+  displayUserScore.innerText = userScore;
+  const displayComputerScore = document.getElementById("computer-score");
+  displayComputerScore.innerText = compScore;
+  checkWinner();
+});
+
+paperButton.addEventListener("click", function () {
+  const playerSelection = "paper";
+  const computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+  const displayUserScore = document.getElementById("user-score");
+  displayUserScore.innerText = userScore;
+  const displayComputerScore = document.getElementById("computer-score");
+  displayComputerScore.innerText = compScore;
+  checkWinner();
+});
+
+scissorsButton.addEventListener("click", function () {
+  const playerSelection = "scissors";
+  const computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+  const displayUserScore = document.getElementById("user-score");
+  displayUserScore.innerText = userScore;
+  const displayComputerScore = document.getElementById("computer-score");
+  displayComputerScore.innerText = compScore;
+  checkWinner();
+});
+
 function computerPlay() {
   const choiceArray = ["rock", "paper", "scissors"];
   const randomChoice = Math.floor(Math.random() * 3);
